5ec02bfbd54891c856d16f10979f45f736cceab8
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-05-25 09:37:06 -0500

    first commit

---
+++ model/entidad/connect.php
@@ -0,0 +1,19 @@
+<?php
+    class connect{
+        private $path;
+        protected $data;
+        function __construct($path){
+            $this->path = $path;
+            $this->data = json_decode(file_get_contents($this->path), true);
+        }
+        public function getData(){
+            return $this->data;
+        }
+        public function postData(){
+            array_unshift($this->data[func_get_arg(0)], func_get_arg(1));
+            $f = fopen($this->path, "w+");
+            fwrite($f, json_encode($this->getData(),JSON_PRETTY_PRINT));
+            fclose($f);
+        }
+    }
+?>
